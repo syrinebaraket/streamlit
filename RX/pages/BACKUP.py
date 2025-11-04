ccbc53a1e026873f943cf2110edfc400bd227d8f
Syrine Baraket <62857679+syrinebaraket@users.noreply.github.com>
2025-11-04 14:40:13 +0100

    Update BACKUP.py

--- RX/pages/BACKUP.py
+++ RX/pages/BACKUP.py
@@ -3,49 +3,7 @@ from PIL import Image
 
 
 st.html(
-"<nav  class="sidebar locked">
-        <div class="logo_items flex">
-          <span class="nav_image">
-            <img src="images/rc.png" alt="logo_img" />
-          </span>
-          <span class="logo_name">CORTEX-RUBIX</span>
-          <i class="bx bx-lock-alt" id="lock-icon" title="Unlock Sidebar"></i>
-          <i class="bx bx-x" id="sidebar-close"></i>
-        </div>
-  
-        <div class="menu_container">
-          <div class="menu_items">
-            
-  
-            <ul class="menu_item">
-              <div class="menu_title flex">
-                <span class="title">Setting</span>
-                <span class="line"></span>
-              </div>
-              <li class="item">
-                <a href="home.html" class="link flex">
-                  <i class="bx bx-home"></i>
-                  <span>Home</span>
-                </a>
-              </li>
-              <li class="item">
-                <a href="ipconfiguration.html" class="link flex">
-                  <i class="bx bx-cog"></i>
-                  <span>IP Configuration</span>
-                </a>
-              </li>
-              
-              
-            </ul>
-          </div>
-  
-         
-        </div>
-      </nav> 
-  
-
-
-    <div class="productContainer">
+"<div class="productContainer">
         <div class="card">
             <div class="header" style="height: 17rem;"> <img src="images/CortexLogo.png" alt="Product" /> </div>
             <div class="footer">
@@ -66,6 +24,6 @@ st.html(
         </div>
             </div>
             <script src="navscript.js" defer></script>"
-    
 )
+
 
