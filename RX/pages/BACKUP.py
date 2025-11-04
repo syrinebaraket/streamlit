7a259c5e058246ab036bab6937e922c4dcb4de10
Syrine Baraket <62857679+syrinebaraket@users.noreply.github.com>
2025-11-04 14:50:12 +0100

    Update BACKUP.py

--- RX/pages/BACKUP.py
+++ RX/pages/BACKUP.py
@@ -172,7 +172,7 @@ st.markdown(
 st.html(
 '''<div class="productContainer">
         <div class="card">
-            <div class="header" style="height: 17rem;"> <img src="images/CortexLogo.png" alt="Product" /> </div>
+            <div class="header" style="height: 17rem;"> <img src="pictures/CortexLogo.png" alt="Product" /> </div>
             <div class="footer">
                 <div class="title">
                     <h2>CORTEX</h2>
@@ -181,7 +181,7 @@ st.html(
             </div>
         </div>
         <div class="card">
-            <div class="header" style="height: 17rem;"> <img src="images/Rubix.png" alt="Rubix"> </div>
+            <div class="header" style="height: 17rem;"> <img src="pictures/Rubix.png" alt="Rubix"> </div>
             <div class="footer">
                 <div class="title">
                     <h2>RUBIX</h2>
@@ -192,6 +192,7 @@ st.html(
             </div>
             <script src="navscript.js" defer></script>'''
 )
+
 
 
 
