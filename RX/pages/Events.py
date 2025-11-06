d05c3ee7bb72cf96ee1f1c118cbd22aecd7eaa64
Syrine Baraket <62857679+syrinebaraket@users.noreply.github.com>
2025-11-06 13:51:34 +0100

    Update Events.py

--- RX/pages/Events.py
+++ RX/pages/Events.py
@@ -63,7 +63,7 @@ available_statuses = sorted(events_df['EventType'].unique())
 selected_statuses = st.sidebar.multiselect("Status Types", available_statuses, default=available_statuses)
 st.markdown("""
     <style>
-        .st-key-boxC , .st-key-boxCC, .st-key-boxCP{  
+        .st-key-boxC , .st-key-boxCC, .st-key-boxCP, .container2{  
         padding: 27px;
         width:100%;
         border-radius: 8px;
