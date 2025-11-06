e80d1b7abed671d09b2b2eeafe7a7fb6475b047e
Syrine Baraket <62857679+syrinebaraket@users.noreply.github.com>
2025-11-06 15:11:46 +0100

    Update Events.py

--- RX/pages/Events.py
+++ RX/pages/Events.py
@@ -226,11 +226,8 @@ else:
                     color_discrete_sequence=color_palettes.get(param, px.colors.qualitative.Plotly))
                 with container2:
                     cols[j].plotly_chart(fig, use_container_width=True)
-                     fig.add_shape(
-                                  line=dict(
-                                      #color="RoyalBlue",
-                                      color="limegreen", #named colors from https://stackoverflow.com/a/72502441/8508004
-                                      width=2, ))
+                    fig.add_shape(
+                                  line=dict( color="limegreen"))
 
 
 
